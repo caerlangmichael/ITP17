482251b205614d41b58e1d3db364ea44cfc6d919
Jan Michael Caerlang <58109924+janmichaelcaerlang@users.noreply.github.com>
2024-07-18 04:25:41 +0800

    Added Features

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HELLO PHP</title>
+    <title>HELLO LARAVEL</title>
 </head>
 <body>
-    <h1>HELLO WORLD in PHP</h1>
+    
+
+    <h1>HELLO WORLD in LARAVEL</h1>
+
+    <h1>HELLO WORLD in GITHUB</h1>
+
+    <h1>HELLO WORLD in GIT</h1>
 </body>
 </html>
